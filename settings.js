66050bdf56f3ed7ba4c0e0dd7ce3557694204e35
dove23-ux <edrinevslmaths@gmail.com>
2025-06-08 08:03:14 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Dovey eddixvian', // Your name
+  botOwner: 'Eddixvian', // Your name
   ownerNumber: '256743387460', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "private",
